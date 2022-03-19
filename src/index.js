c4189e8a65981a7ad16bbb377f24e703164a8b83
vassilis <vasilisdmr@gmail.com>
2022-03-19 21:08:33 +0100

    Add webpack file and changed the indentation.
    In this commit the webpack development file created and the indentation changed to
    4 spaces.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import render from "react-render";
 
 const Hi = () => {
-  return <p>Hi!</p>;
+    return <p>Hi!</p>;
 };
 
 render(<Hi />, document.getElementById("app"));
