d7d5a1de5dca28904ff6df4dcaad5d6d5948f3ed
vassilis <vasilisdmr@gmail.com>
2022-03-22 22:35:27 +0100

    Correct a typo.
    Corrected the useage of "state" to "action".

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,5 +1,5 @@
 export default function courseReducer(state = [], action) {
-    switch (state.type) {
+    switch (action.type) {
         case "CREATE_COURSE":
             return [...state, { ...action.course }];
         default:
