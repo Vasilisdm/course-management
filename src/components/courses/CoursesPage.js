a356fd859961e844889f43a7d0b2db5887d168ba
vassilis <vasilisdmr@gmail.com>
2022-03-22 22:54:32 +0100

    Add display for the courses.
    In this commit the code was modified in order to display the courses the user inserts,
    and the courses property added to prop types.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -33,6 +33,9 @@ class CoursesPage extends React.Component {
                     value={this.state.course.title}
                 />
                 <input type="submit" value="Save" />
+                {this.props.courses.map((course) => (
+                    <div key={course.title}>{course.title}</div>
+                ))}
             </form>
         );
     }
@@ -40,6 +43,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
     dispatch: PropTypes.func.isRequired,
+    courses: PropTypes.array.isRequired,
 };
 
 function mapStateToProps(state) {
