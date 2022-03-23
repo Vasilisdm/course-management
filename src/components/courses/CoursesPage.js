2df38da72864c92ec2e017fd53584ad55aa5fee3
vassilis <vasilisdmr@gmail.com>
2022-03-23 20:33:43 +0100

    Add bindActionCreators.
    In this commit the bindActionCreators was imported and used.
    bindActionCreators binds all the actions found in the courseActions that's
    why I changed the name of the property as well.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 class CoursesPage extends React.Component {
     constructor(props) {
         super(props);
@@ -19,7 +20,7 @@ class CoursesPage extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.props.createCourse(this.state.course);
+        this.props.actions.createCourse(this.state.course);
     };
 
     render() {
@@ -42,7 +43,7 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-    createCourse: PropTypes.func.isRequired,
+    actions: PropTypes.object.isRequired,
     courses: PropTypes.array.isRequired,
 };
 
@@ -54,7 +55,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        createCourse: (course) => dispatch(courseActions.createCourse(course)),
+        actions: bindActionCreators(courseActions, dispatch),
     };
 }
 
