de00c695c8ea0e288e893279862471741b32a87d
vassilis <vasilisdmr@gmail.com>
2022-03-27 16:17:00 +0200

    Add author actions and reducer.
    Load authors action and reducer created and used in courses page,
    courses reducer added to rootReducer, and
    in coures page the state of courses modified to include the name of the
    author

--- src/components/courses/CourseList.js
+++ src/components/courses/CourseList.js
@@ -5,10 +5,12 @@ import { Link } from "react-router-dom";
 const CourseList = ({ courses }) => (
     <table className="table">
         <thead>
-            <th />
-            <th>Title</th>
-            <th>Author</th>
-            <th>Category</th>
+            <tr>
+                <th />
+                <th>Title</th>
+                <th>Author</th>
+                <th>Category</th>
+            </tr>
         </thead>
         <tbody>
             {courses.map((course) => {
@@ -30,7 +32,7 @@ const CourseList = ({ courses }) => (
                                 {course.title}
                             </Link>
                         </td>
-                        <td>{course.authorId}</td>
+                        <td>{course.authorName}</td>
                         <td>{course.category}</td>
                     </tr>
                 );
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
@@ -8,6 +9,9 @@ class CoursesPage extends React.Component {
     componentDidMount() {
         this.props.actions.loadCourses().catch((error) => {
             alert("Loading courses failed" + error);
+        });
+        this.props.actions.loadAuthors().catch((error) => {
+            alert("Loading authors failed" + error);
         });
     }
     render() {
@@ -27,13 +31,33 @@ CoursesPage.propTypes = {
 
 function mapStateToProps(state) {
     return {
-        courses: state.courses,
+        courses:
+            state.authors.length === 0
+                ? []
+                : state.courses.map((course) => {
+                      return {
+                          ...course,
+                          authorName: state.authors.find(
+                              (author) => author.id === course.authorId
+                          ).name,
+                      };
+                  }),
+        authors: state.authors,
     };
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        actions: bindActionCreators(courseActions, dispatch),
+        actions: {
+            loadCourses: bindActionCreators(
+                courseActions.loadCourses,
+                dispatch
+            ),
+            loadAuthors: bindActionCreators(
+                authorActions.loadAuthors,
+                dispatch
+            ),
+        },
     };
 }
 
