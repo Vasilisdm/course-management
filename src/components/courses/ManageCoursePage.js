f3a9f21f326416758b03601c11cf424be960369b
vassilis <vasilisdmr@gmail.com>
2022-03-28 22:17:37 +0200

    Refactor ManageCoursePage
    In this commit the mapDispatchToProps was modified
    to be an object, unnecessary code was removed and the component
    refactored accordingly.

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -3,20 +3,19 @@ import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
 
 class ManageCoursePage extends React.Component {
     componentDidMount() {
-        const { courses, authors, actions } = this.props;
+        const { courses, authors, loadCourses, loadAuthors } = this.props;
 
         if (courses.length === 0) {
-            actions.loadCourses().catch((error) => {
+            loadCourses().catch((error) => {
                 alert("Loading courses failed" + error);
             });
         }
 
         if (authors.length === 0) {
-            actions.loadAuthors().catch((error) => {
+            loadAuthors().catch((error) => {
                 alert("Loading authors failed" + error);
             });
         }
@@ -31,9 +30,10 @@ class ManageCoursePage extends React.Component {
 }
 
 ManageCoursePage.propTypes = {
-    actions: PropTypes.object.isRequired,
     courses: PropTypes.array.isRequired,
     authors: PropTypes.array.isRequired,
+    loadCourses: PropTypes.func.isRequired,
+    loadAuthors: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -43,19 +43,9 @@ function mapStateToProps(state) {
     };
 }
 
-function mapDispatchToProps(dispatch) {
-    return {
-        actions: {
-            loadCourses: bindActionCreators(
-                courseActions.loadCourses,
-                dispatch
-            ),
-            loadAuthors: bindActionCreators(
-                authorActions.loadAuthors,
-                dispatch
-            ),
-        },
-    };
-}
+const mapDispatchToProps = {
+    loadCourses: courseActions.loadCourses,
+    loadAuthors: authorActions.loadAuthors,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
