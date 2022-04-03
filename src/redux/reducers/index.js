48bc8ad3bc046dcf3951380cb288450081d5e358
vassilis <vasilisdmr@gmail.com>
2022-04-03 16:00:17 +0200

    Create apiCallsReducer.
    In this commit an additional action type was added, the related action
    creator was written, the reducer was created and the
    it was passed to the root reducer.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import courses from "./courseReducer";
 import authors from "./authorReducer";
+import apiCallsInProgress from "./apiStatusReducers";
 
 const rootReducer = combineReducers({
     courses: courses,
     authors: authors,
+    apiCallsInProgress: apiCallsInProgress,
 });
 
 export default rootReducer;
