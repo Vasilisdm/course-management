acebe5e2334ae8f9eb65855c37f5eb6b1179c790
vassilis <vasilisdmr@gmail.com>
2022-04-03 17:36:41 +0200

    Configure toast container.
    In this toast container introduced in order
    to display a message when the
    course is saved successfully.

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import CoursesPage from "./courses/CoursesPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
 import ManageCoursePage from "../components/courses/ManageCoursePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
     return (
@@ -19,6 +21,7 @@ const App = () => {
                 <Route path="/course" component={ManageCoursePage} />
                 <Route component={PageNotFound} />
             </Switch>
+            <ToastContainer autoClose={3000} hideProgressBar />
         </div>
     );
 };
